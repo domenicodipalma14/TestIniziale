ad1e753ee866eea585c8446c686f1a86ce02763d
domenicodipalma14 <72913888+domenicodipalma14@users.noreply.github.com>
2020-12-14 19:15:37 +0100

    SignInBoundary.java aggiornato

--- src/boundary/SignInBoundary.java
+++ src/boundary/SignInBoundary.java
@@ -12,8 +12,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import logic.CaricaScenaController;
-import logic.RegistraDatiController;
-import logic.SignInController;
 import bean.SignInBean;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
